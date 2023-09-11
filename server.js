614bd176d84ee9a63c0f86443e467ba68eafc7ce
Daniel Borges <danprog91@gmail.com>
2023-09-11 16:38:48 -0300

    Eliminando console.log

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ server.post('/videos', async (request, reply) => {
 
 server.get('/videos', async (request, reply) => {
     const search = request.query.search
-    console.log(search)
-
 
     const videos = await database.list(search)
     return videos
