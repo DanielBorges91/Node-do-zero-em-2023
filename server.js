0e188216522bfbd1cc1c37f1bee26b70ddf94b27
Daniel Borges <danprog91@gmail.com>
2023-09-11 11:23:40 -0300

    Aplicação com armazenamento em memómia

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-// import { createServer } from 'node:http'
-
-
-// const server = createServer((request, response) => {
-//     response.write('Helo World')
-
-//     return response.end()
-// })
-
-// server.listen(3333)
-
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello World'
+const database = new DatabaseMemory()
+
+
+server.post('/videos', (request, reply) => {
+
+    const { title, description, duration } = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', (request, reply) => {
+    const search = request.query.search
+    console.log(search)
+
+
+    const videos = database.list(search)
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
